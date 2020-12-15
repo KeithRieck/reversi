74f20625b07a955ecf205072d9bdd1f17cf07994
Keith Rieck <KeithRieck@gmail.com>
2020-12-14 20:36:24 -0600

    rebuild

--- build/org.transcrypt.__runtime__.js
+++ build/org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2020-08-08 13:25:24
+// Transcrypt'ed from Python, 2020-12-14 20:33:47
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
