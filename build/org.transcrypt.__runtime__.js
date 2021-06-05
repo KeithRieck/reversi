32075ba4469375353ec2af26d934c62de0f893bc
Keith Rieck <KeithRieck@gmail.com>
2021-06-05 16:02:28 -0500

    Updated PWA config

--- build/org.transcrypt.__runtime__.js
+++ build/org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2021-06-04 21:00:35
+// Transcrypt'ed from Python, 2021-06-05 16:02:11
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
